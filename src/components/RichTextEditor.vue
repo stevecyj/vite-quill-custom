969138181307c5ccf55cc9281d91f6f31253fd5c
stevecyj <ycsteven.tsao@gmail.com>
2022-03-16 11:48:06 +0800

    1st

---
+++ src/components/RichTextEditor.vue
@@ -0,0 +1,30 @@
+<script>
+import { ref, reactive } from 'vue';
+import { QuillEditor } from '@vueup/vue-quill';
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
+export default {
+    components: {
+        QuillEditor,
+    },
+    setup() {
+        return {};
+    },
+};
+</script>
+
+<template>
+  <QuillEditor
+    theme="snow"
+    toolbar="#custom-toolbar"
+  >
+    <template #toolbar>
+      <div id="custom-toolbar">
+        <button class="ql-bold"></button>
+
+      </div>
+    </template>
+  </QuillEditor>
+</template>
+
+<style>
+</style>
